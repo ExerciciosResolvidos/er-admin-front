2e3bbc5898c0c14ec29c988dcd49239376eb25c1
Luiz Zamboni <luizamboni2002@hotmail.com>
2018-01-31 22:44:06 -0200

    ADD tag three

---
+++ test.js
@@ -0,0 +1,41 @@
+// tags = [{"id":242,"title":"Velocidade Máxima","slug":"velocidade-maxima","description":null,"tag_id":null,"childs":[]},{"id":211,"title":"Umidade Relativa do Ar","slug":"umidade-relativa-do-ar","description":null,"tag_id":null,"childs":[]},{"id":250,"title":"Transmissão de Movimento","slug":"transmissao-de-movimento","description":null,"tag_id":null,"childs":[]},{"id":235,"title":"Teorema de Stevin","slug":"teorema-de-stevin","description":null,"tag_id":null,"childs":[]},{"id":225,"title":"Teorema de Arquimedes","slug":"teorema-de-arquimedes","description":null,"tag_id":null,"childs":[]},{"id":131,"title":"Taxa de variação média","slug":"taxa-de-variacao-media","description":null,"tag_id":null,"childs":[]},{"id":163,"title":"Taxa de Variação","slug":"taxa-de-variacao","description":null,"tag_id":null,"childs":[]},{"id":199,"title":"Sistema Internacional de Unidades","slug":"sistema-internacional-de-unidades","description":null,"tag_id":null,"childs":[]},{"id":271,"title":"Sequências","slug":"sequencias","description":null,"tag_id":null,"childs":[]},{"id":263,"title":"Ressonância","slug":"ressonancia","description":null,"tag_id":null,"childs":[]},{"id":210,"title":"Rendimento","slug":"rendimento","description":null,"tag_id":null,"childs":[]},{"id":193,"title":"Relação entre grandezas","slug":"relacao-entre-grandezas","description":null,"tag_id":null,"childs":[]},{"id":26,"title":"Química","slug":"quimica","description":"Química é a ciência que estuda a composição, estrutura, propriedades da matéria, as mudanças sofridas por ela durante as reações químicas e sua relação com a energia.\r\nO desenvolvimento desta ciência teve como base as observações de experimentos, sendo portanto, considerada uma ciência experimental. ","tag_id":null,"childs":[{"id":19,"title":"Estequiometria","slug":"estequiometria","description":"A estequiometria baseia-se na lei da conservação das massas e na lei das proporções definidas (ou lei da composição constante), e na lei das proporções múltiplas. Em geral, as reações químicas combinam proporções definidas de compostos químicos. Já que a matéria não pode ser criada ou destruída, a quantia de cada elemento deve ser a mesma antes, durante e após a reação. Por exemplo, a quantia de um elemento A no reagente deve ser igual à quantia do mesmo elemento no produto.\r\nA estequiometria é usada frequentemente para balancear equações químicas. Por exemplo, os dois gases diatômicos hidrogênio e oxigênio podem combinar-se para formar um líquido, água, em uma reação exotérmica, como descrita na Equação:\r\n$$H_2 + O_2 \\longrightarrow H_2O$$","tag_id":26,"childs":[{"id":16,"title":"Mols","slug":"mols","description":null,"tag_id":19,"childs":[]}]},{"id":31,"title":"Concentração","slug":"concentracao","description":"Em Química, concentração é a razão entre a quantidade ou a massa de uma substância e o volume do solvente em que esse composto se encontra dissolvido. A forma com que uma substância se distribui em outra por unidade de referência. Essa unidade de referência pode ser o volume ou massa de solução ou até de solvente.\r\nPara que haja solução é necessário que o solvente \"envolva\" as partículas de soluto, tal processo é chamado de solvatação. Daí é que estamos falando em concentração, quanto mais partículas de soluto são envolvidas pelo solvente por unidade (mL, L, g, kg), mais concentrada é a solução e quanto menos partículas de soluto são solvatadas pelo solvente, menos concentrada é a solução.","tag_id":26,"childs":[]},{"id":74,"title":"Titulação","slug":"titulacao","description":"Titulação (ou Volumetria) é uma técnica comum de laboratório em análise química quantitativa, usado para determinar a concentração de um reagente conhecido. O método consiste em reagir completamente um volume conhecido de uma amostra com um volume determinado de um reagente de natureza e concentração conhecida (solução padrão). A substância de interesse em qualquer determinação recebe o nome de analito. A espécie química com a maior concentração definida recebe o nome de titulante, que é, em geral, uma solução obtida a partir de um padrão primário, podendo ser um sal ou uma substância gerada na solução que se deseja valorar. A solução a ter sua concentração determinada recebe o nome de titulado.","tag_id":26,"childs":[]},{"id":75,"title":"Tabela periódica","slug":"tabela-periodica","description":"A tabela periódica dos elementos químicos é a disposição sistemática dos elementos, na forma de uma tabela, em função de suas propriedades. É muito útil para prever as características e propriedades dos elementos químicos. Permite, por exemplo, prever o comportamento de átomos e das moléculas deles formadas, ou entender por que certos átomos são extremamente reativos enquanto outros são praticamente inertes. Permite prever propriedades como eletronegatividade, raio iônico e energia de ionização. Os grupos rosa e o vermelho meio magenta são grupos dos Lantanoides e Actinoides, também conhecidos como Lantanídeos e Actinídeos.","tag_id":26,"childs":[]}]},{"id":260,"title":"Quantidade de Movimento","slug":"quantidade-de-movimento","description":null,"tag_id":null,"childs":[]},{"id":220,"title":"Propriedades da água","slug":"propriedades-da-agua","description":null,"tag_id":null,"childs":[]},{"id":195,"title":"Projeção Ortogonal","slug":"projecao-ortogonal","description":null,"tag_id":null,"childs":[]},{"id":185,"title":"Projeção","slug":"projecao","description":null,"tag_id":null,"childs":[]},{"id":227,"title":"Produção de energia elétrica","slug":"producao-de-energia-eletrica","description":null,"tag_id":null,"childs":[]},{"id":140,"title":"Princípio Fundamental da Contagem","slug":"principio-fundamental-da-contagem","description":null,"tag_id":null,"childs":[]},{"id":247,"title":"Princípio de Pascal","slug":"principio-de-pascal","description":null,"tag_id":null,"childs":[]},{"id":246,"title":"Pressão Atmosférica","slug":"pressao-atmosferica","description":null,"tag_id":null,"childs":[]},{"id":168,"title":"Posição de um algarismo","slug":"posicao-de-um-algarismo","description":null,"tag_id":null,"childs":[]},{"id":249,"title":"Polias","slug":"polias","description":null,"tag_id":null,"childs":[]},{"id":258,"title":"Pêndulo","slug":"pendulo","description":null,"tag_id":null,"childs":[]},{"id":137,"title":"Ordem de Grandeza","slug":"ordem-de-grandeza","description":null,"tag_id":null,"childs":[]},{"id":248,"title":"Ondulatória","slug":"ondulatoria","description":null,"tag_id":null,"childs":[]},{"id":223,"title":"Ondas Eletromagnéticas","slug":"ondas-eletromagneticas","description":null,"tag_id":null,"childs":[]},{"id":186,"title":"Notação Científica","slug":"notacao-cientifica","description":null,"tag_id":null,"childs":[]},{"id":228,"title":"Movimento Browniano","slug":"movimento-browniano","description":null,"tag_id":null,"childs":[]},{"id":237,"title":"Meio Ambiente","slug":"meio-ambiente","description":null,"tag_id":null,"childs":[]}]
+
+
+
+tags = [
+  { "id":16,"title":"Mols","slug":"mols", "childs":[] },
+  
+
+  { "id":26,"title":"Química","slug":"quimica",
+    "childs":[
+      {"id":19,"title":"Estequiometria","slug":"estequiometria","tag_id":26,
+       "childs":[
+        {"id":16,"title":"Mols","slug":"mols","tag_id":19,"childs":[]}
+       ]
+      }
+    ]
+  }
+]
+
+function linkTags(tags) {
+  return tags.filter(t => 
+    !findInTagThree(t.id, tags)
+  )
+}
+
+function findInTagThree(tagId, tags, first = true) {
+  // console.log(tags)
+  for(const compareTag of tags) {
+    if(!first && compareTag.id === tagId)
+      return true
+    
+      
+    if(findInTagThree(tagId, compareTag.childs, false)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+console.log(linkTags(tags))
